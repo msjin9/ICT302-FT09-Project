958d5e9a23522f9fa9a22b8d65c8d397acdd86a3
airglow923 <p.hyundeok76@gmail.com>
2021-02-07 11:15:49 +0800

    add server

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const frontPath = resolve(currentDir, 'views/');
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:8080',
+};
+
+app.use(express.static(frontPath));
+app.use(morgan('combined'));
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(frontPath, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
