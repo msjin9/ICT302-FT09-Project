e06e2d963a5916d518c93bb3b4273b086e26e4b1
airglow923 <p.hyundeok76@gmail.com>
2021-02-08 10:00:33 +0800

    rewrite powershell script as batch

---
+++ deploy.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM  change working directory to the script directory
+cd %~dp0
+
+REM  go to client source
+cd client
+
+REM  transpile client source with babel
+call npm run build
+
+REM  copy built source to server static source
+robocopy dist ..\server\src\views /MT /E /XD
+
+REM  go to server source
+cd ../server
+
+REM  run server
+npm run serve
