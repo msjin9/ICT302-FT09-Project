fcc7f1bc3785ab45764632f7af6a9c96a433b4f8
airglow923 <p.hyundeok76@gmail.com>
2021-02-08 12:11:08 +0800

    edit deploy scripts

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,9 @@ cd %~dp0
 REM  go to client source
 cd client
 
+REM  install dependencies
+npm ci
+
 REM  transpile client source with babel
 call npm run build
 
@@ -14,6 +17,9 @@ robocopy dist ..\server\src\views /MT /E /XD
 
 REM  go to server source
 cd ../server
+
+REM  install dependencies
+npm ci
 
 REM  run server
 npm run serve
